613599aa361ade49c133785a45687aacea756ba5
Vansh Nagpal <vanshbharatnagpal@gmail.com>
2026-04-03 21:32:44 +0530

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ st.markdown("""
 # ---------------- LOAD MODEL ----------------
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("model.keras")
+    return tf.keras.models.load_model("model.h5")
 
 model = load_model()
 
