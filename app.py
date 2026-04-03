431e33bc5970c835792d084e13a155006390ceca
Vansh Nagpal <vanshbharatnagpal@gmail.com>
2026-04-03 21:08:19 +0530

    1

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ st.markdown("""
 # ---------------- LOAD MODEL ----------------
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("model.h5")
+    return tf.keras.models.load_model("model.h5", compile=False)
 
 model = load_model()
 
